c41603a9538011dd17c41c1553265b2024d40c82
Pierre Lacave <pierre@lacave.me>
2015-08-21 09:49:03 +0100

    firstish stats calculation

--- Window/build.sbt
+++ Window/build.sbt
@@ -1,12 +1,22 @@
-name := "Window Aggregate"
+
+name := "WindowAggregate"
 
 version := "1.2"
 
 scalaVersion := "2.10.4"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-sql" % "1.4.1",
-  "org.apache.spark" %% "spark-hive-thriftserver" % "1.4.1",
-  "org.apache.spark" %% "spark-streaming" % "1.4.1",
-  "org.apache.spark" %% "spark-streaming-kafka" % "1.4.1"
+  "org.apache.spark" %% "spark-sql" % "1.4.1" % "provided",
+  "org.apache.spark" %% "spark-hive-thriftserver" % "1.4.1" % "provided",
+  "org.apache.spark" %% "spark-streaming" % "1.4.1" % "provided",
+  "org.apache.spark" %% "spark-streaming-kafka" % "1.4.1",
+  "org.json4s" %% "json4s-native" % "3.2.10"
 )
+
+// META-INF discarding
+mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
+{
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+}
--- Window/src/main/scala/network.scala
+++ Window/src/main/scala/network.scala
@@ -5,6 +5,10 @@ import org.apache.spark.storage.StorageLevel
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.hive.thriftserver._
 import org.apache.spark.streaming.kafka.KafkaUtils
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import org.json4s.native.JsonParser
+
 
 case class Word(name: String, count: Int)
 
@@ -15,7 +19,7 @@ object Network {
       System.exit(1)
     }
 
-    val conf = new SparkConf().setAppName("Kafka Aggregate")
+    val conf = new SparkConf().setAppName("Window Aggregate")
     val ssc = new StreamingContext(conf, Seconds(1))
     val sqlContext = new SQLContext(ssc.sparkContext)
     val hiveContext = new HiveContext(ssc.sparkContext)
@@ -27,25 +31,74 @@ object Network {
     val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
 
-    val people = hiveContext.createDataFrame(hiveContext.sparkContext.emptyRDD[Word])
-    people.registerTempTable("windowed")
 
-    lines.window(Minutes(1440)).foreachRDD{ rdd =>
-      if (rdd.count() > 0) {
-        hiveContext.read.json(rdd).toDF().registerTempTable("windowed")
-      }
+    val topicA = lines.map(line => JsonParser.parse(line)).filter { json =>
+      implicit val formats = DefaultFormats
+      (json \ "topic").extract[String].equalsIgnoreCase("a")
     }
 
-    lines.window(Minutes(1), Minutes(1)).foreachRDD { rdd =>
-      if (rdd.count() > 0) {
-        hiveContext.read.json(rdd).toDF().write.mode(SaveMode.Append).parquet("persisted")
-      }
+
+    val topicA_1 = topicA.window(Seconds(1), Seconds(1))
+    topicA_1.foreachRDD{ rdd =>
+      println("Message count in Topic A in Last Second: " + rdd.count())
+    }
+    topicA_1.map { json =>
+      implicit val formats = DefaultFormats
+      ((json \ "messageType").extract[String], 1)
+    }.reduceByKey(_ + _).foreachRDD { messageType =>
+      println("Message count in Topic A Broken by Message Type Last Second: ")
+      messageType.collect().foreach(println)
     }
 
-    lines.window(Seconds(30)).foreachRDD { (rdd, time) =>
-      val all = hiveContext.sql("SELECT count(*) FROM windowed")
-      println("in last 30s: " + rdd.count() + " and total is " + all.first().getLong(0))
+
+
+    val topicA_10 = topicA.window(Seconds(10), Seconds(5))
+    topicA_10.foreachRDD{ rdd =>
+      println("Message count in Topic A in Last 10 Seconds: " + rdd.count())
     }
+    topicA_10.map { json =>
+      implicit val formats = DefaultFormats
+      ((json \ "messageType").extract[String], 1)
+    }.reduceByKey(_ + _).foreachRDD { messageType =>
+      println("Message count in Topic A Broken by Message Type Last 10 Seconds: ")
+      messageType.collect().foreach(println)
+    }
+
+
+
+    val topicA_10m = topicA.window(Minutes(1), Seconds(10))
+    topicA_10m.foreachRDD{ rdd =>
+      println("Message count in Topic A in last Minute: " + rdd.count())
+    }
+    topicA_10m.map { json =>
+      implicit val formats = DefaultFormats
+      ((json \ "messageType").extract[String], 1)
+    }.reduceByKey(_ + _).foreachRDD { messageType =>
+      println("Message count in Topic A Broken by Message Type Last Minute: ")
+      messageType.collect().foreach(println)
+    }
+
+
+
+
+
+
+
+//    lines.window(Seconds(1)).foreachRDD{ rdd =>
+//      if (rdd.count() > 0) {
+//        hiveContext.read.json(rdd).toDF().first().get()
+//        hiveContext.read.json(rdd).toDF().registerTempTable("windowed")
+//        println(hiveContext.sql("select count(*) from windowed group by a"))
+//      }
+//    }
+//
+//    lines.window(Seconds(5), Seconds(5)).foreachRDD { rdd =>
+//      if (rdd.count() > 0) {
+//        hiveContext.read.json(rdd).toDF().write.mode(SaveMode.Append).parquet("persisted")
+//        hiveContext.sql("CREATE TABLE IF NOT EXISTS persisted USING org.apache.spark.sql.parquet OPTIONS ( path '/Users/zoidberg/Documents/Spark/spark-1.4.1-bin-hadoop2.6/SparkPlayground/Window/persisted')")
+//        hiveContext.sql("REFRESH TABLE persisted")
+//      }
+//    }
 
     ssc.start()             // Start the computation
     ssc.awaitTermination()  // Wait for the computation to terminate
